4b23155d5489f6a33c696b59d6ea424facd1defb
Anuj <ashah812@gmail.com>
2020-05-27 17:45:31 -0400

    update use effect

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Messenger } from "./components/Messenger/Messenger";
 import Login from "./components/Login/Login";
-import { connect, closeSocket } from "./socket";
+import { connect } from "./socket";
 import { Message } from "./components/MessageContainer.tsx/MessageContainer";
 import styled from "styled-components/macro";
 
@@ -13,15 +13,16 @@ const App = () => {
   const [email, setEmail] = useState("");
   const [messageHistory, setMessageHistory] = useState<Message[]>([]);
 
-  useEffect(() => {
-    connect((msg) => {
-      setMessageHistory((oldMessages) => [
-        ...oldMessages,
-        JSON.parse(msg.data),
-      ]);
-    });
-    return closeSocket();
-  }, []);
+  useEffect(
+    () =>
+      connect((msg) => {
+        setMessageHistory((oldMessages) => [
+          ...oldMessages,
+          JSON.parse(msg.data),
+        ]);
+      }),
+    []
+  );
 
   return (
     <AppWrapper>
